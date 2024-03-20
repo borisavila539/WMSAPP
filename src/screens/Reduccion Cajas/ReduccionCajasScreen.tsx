e0ccdf6cfeb390cef291dcf8e2fc3b66959f525a
borisavila539 <68365374+borisavila539@users.noreply.github.com>
2024-03-20 15:19:42 -0600

    reduccion de cajas

--- src/screens/Reduccion Cajas/ReduccionCajasScreen.tsx
+++ src/screens/Reduccion Cajas/ReduccionCajasScreen.tsx
@@ -1,10 +1,10 @@
 import { StackScreenProps } from '@react-navigation/stack'
-import React, { FC, useEffect, useRef, useState } from 'react'
+import React, { FC, useContext, useEffect, useRef, useState } from 'react'
 import { RootStackParams } from '../../navigation/navigation'
-import { ActivityIndicator, FlatList, StyleSheet, Switch, TextInput, TouchableOpacity, View } from 'react-native'
+import { ActivityIndicator, FlatList, Modal, Pressable, StyleSheet, Switch, TextInput, TouchableOpacity, View } from 'react-native'
 import { Text } from 'react-native-elements'
 import Header from '../../components/Header'
-import { black, blue, grey, orange } from '../../constants/Colors'
+import { black, blue, grey, navy, orange } from '../../constants/Colors'
 import Icon from 'react-native-vector-icons/FontAwesome5'
 import { WmSApi } from '../../api/WMSApi'
 import SoundPlayer from 'react-native-sound-player'
@@ -12,6 +12,8 @@ import MyAlert from '../../components/MyAlert'
 import { GrupoLineasDiariointerface, LineasDiariointerface } from '../../interfaces/LineasDiarioInterface'
 import { RefreshControl } from 'react-native-gesture-handler'
 import Printers from '../../components/Printers'
+import { PrinterInterface } from '../../interfaces/PrintersInterface'
+import { WMSContext } from '../../context/WMSContext'
 
 type props = StackScreenProps<RootStackParams, "ReduccionCajasScreen">
 
@@ -25,6 +27,12 @@ export const ReduccionCajasScreen: FC<props> = ({ navigation }) => {
   const [tipoMensaje, setTipoMensaje] = useState<boolean>(false);
   const [mensajeAlerta, setMensajeAlerta] = useState<string>('');
   const [Lineas, setLineas] = useState<GrupoLineasDiariointerface[]>([])
+  const [Impresoras, setImpresoras] = useState<PrinterInterface[]>([]);
+  const [ShowImpresoras, setShowImpresoras] = useState<boolean>(false);
+  const { WMSState } = useContext(WMSContext);
+  const [ubicacion, setubicacion] = useState<string>('')
+  const textInputRef2 = useRef<TextInput>(null);
+
 
   const getData = async () => {
     try {
@@ -113,7 +121,7 @@ export const ReduccionCajasScreen: FC<props> = ({ navigation }) => {
 
     return (
       <View style={style.containerCard} >
-        <View style={[style.card,{ backgroundColor: blue }]}>
+        <View style={[style.card, { backgroundColor: blue }]}>
           <View style={{ width: '80%' }}>
             <Text style={[style.textCard, { fontWeight: 'bold' }]}>{item.items[0].itemid} *{item.items[0].inventcolorid}</Text>
 
@@ -152,12 +160,50 @@ export const ReduccionCajasScreen: FC<props> = ({ navigation }) => {
       </View>
     )
   }
-  const onPress = () =>{
-    
+
+  const getImpresoras = async () => {
+    try {
+      await WmSApi.get<PrinterInterface[]>('Impresoras').then(resp => {
+        setImpresoras(resp.data)
+      })
+    } catch (err) {
+      console.log(err)
+    }
   }
+
+  const onSelectPrint = async (item: PrinterInterface) => {
+    if (ubicacion.length > 0) {
+      try {
+        await WmSApi.get<string>(`ImprimirEtiquetaReduccion/${IMBoxCode}/010101-01/${WMSState.usuario}/${item.iM_IPPRINTER}`)
+      } catch (err) {
+        console.log(err)
+      }
+    }else{
+      setMensajeAlerta('Seleccione una ubicacion')
+      setTipoMensaje(false)
+      setShowMensajeAlerta(true)
+    }
+
+
+    setShowImpresoras(false)
+
+  }
+
+  const renderPrint = (item: PrinterInterface, index: number) => {
+    return (
+      <View style={{ width: '100%', borderWidth: 1, flex: 1, padding: 10, borderRadius: 15, marginBottom: 3 }}>
+        <Pressable onPress={() => onSelectPrint(item)}>
+          <Text style={{ textAlign: 'center' }}>{item.iM_DESCRIPTION_PRINTER}</Text>
+        </Pressable>
+      </View>
+    )
+
+  }
+
 
   useEffect(() => {
     textInputRef.current?.focus()
+    getImpresoras()
   }, [])
 
   useEffect(() => {
@@ -168,7 +214,7 @@ export const ReduccionCajasScreen: FC<props> = ({ navigation }) => {
     if (barCode.slice(0, 2) == "IM") {
       setIMBoxCode(barCode)
       setbarcode('')
-      
+
     } else {
       if (barCode.length == 13 && !cargando && add) {
         AgregarEliminarArticulo('ADD')
@@ -191,7 +237,7 @@ export const ReduccionCajasScreen: FC<props> = ({ navigation }) => {
           onChangeText={(value) => { setbarcode(value) }}
           value={barCode}
           style={style.input}
-          onBlur={() => textInputRef.current?.focus()}
+          onBlur={() => textInputRef2.current?.isFocused() ? null : textInputRef.current?.focus()}
           placeholder={add ? 'Escanear Ingreso...' : 'Escanear Reduccion...'}
 
         />
@@ -205,15 +251,30 @@ export const ReduccionCajasScreen: FC<props> = ({ navigation }) => {
 
       </View>
 
+      <View style={style.textInput}>
+        <TextInput
+          ref={textInputRef2}
+          onChangeText={(value) => { setubicacion(value) }}
+          value={ubicacion}
+          style={[style.input, { width: '100%' }]}
+          placeholder='ubicacion'
+          onBlur={() => textInputRef.current?.isFocused() ? null : textInputRef2.current?.focus()}
+        />
+      </View>
+
       {
         IMBoxCode != '' &&
-        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-          <Icon name='box-open' size={30} color={add ? '#77D970' : '#CD4439'} />
-          <Text style={{ fontWeight: 'bold', color: add ? '#77D970' : '#CD4439' }}> {IMBoxCode}</Text>
-          <TouchableOpacity style={{ backgroundColor: orange, width: '90%', borderRadius: 10, paddingVertical: 8, alignItems: 'center' }} onPress={onPress}>
-                <Text style={{ color: grey }}>Imprimir Etiqueta</Text>
-            </TouchableOpacity>
-        </View>
+        <>
+          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+            <Icon name='box-open' size={30} color={add ? '#77D970' : '#CD4439'} />
+            <Text style={{ fontWeight: 'bold', color: add ? '#77D970' : '#CD4439' }}> {IMBoxCode}</Text>
+
+          </View>
+          <TouchableOpacity style={{ backgroundColor: orange, width: '90%', borderRadius: 10, paddingVertical: 8, alignItems: 'center' }}
+            onPress={() => setShowImpresoras(true)}>
+            <Text style={{ color: grey }}>Imprimir Etiqueta</Text>
+          </TouchableOpacity>
+        </>
       }
 
       {
@@ -233,7 +294,33 @@ export const ReduccionCajasScreen: FC<props> = ({ navigation }) => {
           </View>
       }
       <MyAlert visible={showMensajeAlerta} tipoMensaje={tipoMensaje} mensajeAlerta={mensajeAlerta} onPress={() => { setShowMensajeAlerta(false); textInputRef.current?.focus(); }} />
+      <Modal visible={ShowImpresoras} transparent={true}>
+        <View style={style.modal}>
+          <View style={style.constainerModal}>
+            <View style={{ width: '100%', flexDirection: 'row', justifyContent: 'space-between', marginBottom: 3 }}>
+              <View></View>
+              <View>
+                <Text style={style.text}>
+                  Impresoras
+                </Text>
+              </View>
+              <View>
+                <TouchableOpacity onPress={() => setShowImpresoras(false)} style={{ padding: 5 }}>
+                  <Icon name='times' size={15} color={black} />
+                </TouchableOpacity>
+              </View>
+            </View>
 
+            <FlatList
+              data={Impresoras}
+              keyExtractor={(item) => item.iM_DESCRIPTION_PRINTER}
+              renderItem={({ item, index }) => renderPrint(item, index)}
+              style={{ width: '90%' }}
+              showsVerticalScrollIndicator={false}
+            />
+          </View>
+        </View>
+      </Modal>
     </View>
   )
 }
@@ -272,6 +359,28 @@ const style = StyleSheet.create({
   input: {
     width: '75%',
     textAlign: 'center'
+  },
+  modal: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 1,
+    backgroundColor: '#00000099',
+
+  },
+  constainerModal: {
+    width: '90%',
+    backgroundColor: grey,
+    alignItems: 'center',
+    borderRadius: 10,
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    maxHeight: 400
+  },
+  text: {
+    fontWeight: 'bold',
+    marginTop: 10,
+    color: navy
   },
 
 })
