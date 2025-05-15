f2caf3a09e9c7fd88c06121ae82e5f8f380c018a
borisavila539 <68365374+borisavila539@users.noreply.github.com>
2025-05-15 10:15:56 -0600

    produccion tela

--- src/constants/api.tsx
+++ src/constants/api.tsx
@@ -1,5 +1,5 @@
 import { WmSApiCaex } from '../api/WmsApiCaex';
-const WMSUrl = 'http://localhost:5001/api/WMS/'//produccion 10.100.2.17:8095 --pruebaÑ8096
+const WMSUrl = 'http://10.100.2.17:8095/api/WMS/'//produccion 10.100.2.17:8095 --pruebaÑ8096
 const WMSUrlCaex = 'http://10.100.2.17:8095/api/WMSCAEX/'//produccion 10.100.2.17:8095 --pruebaÑ8096
 const WMSMB = 'http://10.100.2.17:8095/api/WMSMB/'//produccion 10.100.2.17:8095 --pruebaÑ8096
 //const WMSMB = 'http://localhost:13274/api/WMSMB/'
