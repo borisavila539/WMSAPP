92bf0140f1a36a1345d1aef93a184cfadb0c0ce9
eliBueso <107525077+eliBueso@users.noreply.github.com>
2025-10-13 15:20:19 -0600

    Reimpresion etiquetas y serigrafia (#7)
    * Cambios de ReimpresionEtiqueta y Serigrafia
    
    Principal-> Reimpresion de etiquetas
    
    * Update ReimpresionEtiquetas.tsx
    
    * SerigrafiaApiProductiva
    
    * Update api.tsx
    
    ---------
    
    Co-authored-by: borisavila539 <68365374+borisavila539@users.noreply.github.com>

--- src/constants/api.tsx
+++ src/constants/api.tsx
@@ -1,8 +1,10 @@
 import { WmSApiCaex } from '../api/WmsApiCaex';
 const WMSUrl = 'http://10.100.2.17:8095/api/WMS/'//produccion 10.100.2.17:8095 --pruebaÑ8096
 const WMSUrlCaex = 'http://10.100.2.17:8095/api/WMSCAEX/'//produccion 10.100.2.17:8095 --pruebaÑ8096
+const WMSSerigrafia = 'http://10.100.2.17:8095/api/WMSSerigrafia/'//produccion
 const WMSMB = 'http://10.100.2.17:8095/api/WMSMB/'//produccion 10.100.2.17:8095 --pruebaÑ8096
+//const WMSSerigrafia = 'http://localhost:13274/api/WMSSerigrafia/'
 //const WMSMB = 'http://localhost:13274/api/WMSMB/'
 //const WMSUrl ='http://localhost:13274/api/WMS/'
 
-export { WMSUrl,WMSUrlCaex ,WMSMB }
+export { WMSUrl,WMSUrlCaex ,WMSMB, WMSSerigrafia };
