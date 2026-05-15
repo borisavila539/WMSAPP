9dacfccaedf7cdc57408c54b78628067a66b305a
borisavila539 <68365374+borisavila539@users.noreply.github.com>
2026-05-15 10:07:34 -0600

    cambiando a produccion

--- src/constants/api.tsx
+++ src/constants/api.tsx
@@ -1,7 +1,7 @@
 import { WmSApiCaex } from '../api/WmsApiCaex';
 const WMSUrl = 'http://10.100.2.17:8095/api/WMS/'//produccion 10.100.2.17:8095 --pruebaÑ8096
 const WMSUrlCaex = 'http://10.100.2.17:8095/api/WMSCAEX/'//produccion 10.100.2.17:8095 --pruebaÑ8096
-const WMSSerigrafia = 'http://10.100.2.17:8085/api/WMSSerigrafia/'//produccion
+const WMSSerigrafia = 'http://10.100.2.17:8095/api/WMSSerigrafia/'//produccion
 const WMSMB = 'http://10.100.2.17:8095/api/WMSMB/'//produccion 10.100.2.17:8095 --pruebaÑ8096
 // const WMSUrl ='http://localhost:13274/api/WMS/'
 // const WMSSerigrafia = 'http://localhost:13274/api/WMSSerigrafia/'
